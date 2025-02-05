1f03ef3127d3989acc5091531afed89651fc57af
Jess Sevilleja <mjvsevilleja@gmail.com>
2025-02-05 22:41:49 +0800

    added login page and component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,35 @@
+import { Poppins } from "next/font/google";
 import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import { LoginButton } from "@/components/ui/auth/login-button";
+
+const font = Poppins({
+  subsets: ["latin"],
+  weight: ["600"],
+});
 
 export default function Home() {
+  console.log("login page");
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Button size={"lg"}>App Page</Button>
+    <main className="flex h-full flex-col items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800">
+      <div className="space-y-6 text-center">
+        <h1
+          className={cn(
+            "text-4xl font-bold text-white drop-shadow-md",
+            font.className
+          )}
+        >
+          🔐 Auth
+        </h1>
+        <p className="text-white">A simple authentication service</p>
+        <div>
+          <LoginButton>
+            <Button variant="secondary" size="lg">
+              Sign in
+            </Button>
+          </LoginButton>
+        </div>
+      </div>
     </main>
   );
 }
